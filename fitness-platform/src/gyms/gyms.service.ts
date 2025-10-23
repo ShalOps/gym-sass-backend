c9788b5c929b910e678e0b9fe019ddb5115d8c2a
berki6 <bereketfikadu71@gmail.com>
2025-10-23 22:01:22 +0300

    refactor(gyms): improve code formatting based on ts linting

--- fitness-platform/src/gyms/gyms.service.ts
+++ fitness-platform/src/gyms/gyms.service.ts
@@ -1,46 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '../../generated/prisma';
-import { DatabaseService } from 'src/database/database.service'; 
-
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class GymsService {
-  constructor(private readonly databaseservice: DatabaseService){}
+  constructor(private readonly databaseservice: DatabaseService) {}
 
   create(createGymDto: Prisma.GymCreateInput) {
     return this.databaseservice.gym.create({
-      data: createGymDto
-    })
+      data: createGymDto,
+    });
   }
 
   findAll() {
-    return this.databaseservice.gym.findMany()
-
+    return this.databaseservice.gym.findMany();
   }
 
   findOne(id: number) {
-     return this.databaseservice.gym.findUnique({
+    return this.databaseservice.gym.findUnique({
       where: {
         gymId: id,
-      }
-    })
-
+      },
+    });
   }
 
   update(id: number, updateGymDto: Prisma.GymUpdateInput) {
-      return this.databaseservice.gym.update({
+    return this.databaseservice.gym.update({
       where: {
         gymId: id,
       },
       data: updateGymDto,
-    })
+    });
   }
 
   remove(id: number) {
     return this.databaseservice.gym.delete({
       where: {
         gymId: id,
-      }
-    })
+      },
+    });
   }
 }
