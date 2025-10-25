b9dba3f613d24b63396037e564792612524f9556
Nathan Fekade <nathanfekade276@gmail.com>
2025-10-25 14:00:57 +0300

    feat: Add Prisma exception filter and validation pipes

--- fitness-platform/src/filters/prisma-exception.filter.ts
+++ fitness-platform/src/filters/prisma-exception.filter.ts
@@ -1,7 +1,5 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
-// import { Prisma } from '../../generated/prisma';
-import { Prisma } from '@prisma/client';
-
+import { Prisma } from '../../generated/prisma';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaExceptionFilter implements ExceptionFilter {
