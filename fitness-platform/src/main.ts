5620681444b7d367ff6f9844ec8d264d2a426221
berki6 <bereketfikadu71@gmail.com>
2025-10-30 13:10:38 +0300

    feat(main): add imports for NestExpressApplication and path handling

--- fitness-platform/src/main.ts
+++ fitness-platform/src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const UPLOADS_DIR = process.env.UPLOADS_DIR || './uploads';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new PrismaExceptionFilter());
 
@@ -31,6 +35,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useStaticAssets(join(__dirname, '..', UPLOADS_DIR));
 
   await app.listen(process.env.PORT ?? 3000);
 }
