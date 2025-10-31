6b31bbbbab9ab17bd988f06dab835f33cab7e260
Nathan Fekade <nathanfekade276@gmail.com>
2025-10-31 15:40:29 +0300

    feat: add swagger for services api endpoints

--- fitness-platform/src/services/services.controller.ts
+++ fitness-platform/src/services/services.controller.ts
@@ -2,33 +2,51 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpCo
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-services.dto';
 import { UpdateServiceDto } from './dto/update-services.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-
+@ApiTags('services')
 @Controller('services')
 export class ServicesController {
   constructor(private readonly servicesService: ServicesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new service' })
+  @ApiResponse({status: 201, description: 'Service successfully created.' })
+  @ApiResponse({status: 400, description: 'Invalid input data.' })
+  @ApiResponse({ status: 409, description: 'Service with this name already exists in the gym.' })
   create(@Body() createServiceDto: CreateServiceDto) {
     return this.servicesService.create(createServiceDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Find all created services' })
+  @ApiResponse({ status: 200, description: 'List of all services.' })
   findAll() {
     return this.servicesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a service by id' })
+  @ApiResponse({ status: 200, description: 'Service found.' })
+  @ApiResponse({ status: 404, description: 'Service not found.' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.servicesService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a service by id' })
+  @ApiResponse({ status: 200, description: 'Service successfully updated.' })
+  @ApiResponse({ status: 400, description: 'Invalid input data.' })
+  @ApiResponse({ status: 404, description: 'Service not found.' })
+  @ApiResponse({ status: 409, description: 'Updated name conflicts with existing service in the same gym.' })
   update(@Param('id', ParseIntPipe) id: number, @Body() updateServiceDto: UpdateServiceDto) {
     return this.servicesService.update(id, updateServiceDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a service by id' })
+  @ApiResponse({ status: 204, description: 'Service successfully deleted.' })
+  @ApiResponse({ status: 404, description: 'Service not found.' })
   @HttpCode(204)
   async remove(@Param('id', ParseIntPipe) id: number) {
     await this.servicesService.remove(id);
