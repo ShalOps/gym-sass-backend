4d5c462cb111e19f692725e89e80ce56e4eb39aa
berki6 <bereketfikadu71@gmail.com>
2025-10-31 12:23:03 +0300

    feat(app-module):  register MulterModule & UploadsModule for file uploads

--- fitness-platform/src/app.module.ts
+++ fitness-platform/src/app.module.ts
@@ -4,15 +4,21 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { GymsModule } from './gyms/gyms.module';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { ServicesModule } from './services/services.module';
-import { ServiceOptionModule } from './service-option/service-option.module';
-import { GymClassesModule } from './gym-classes/gym-classes.module';
-import { ServiceOptionAssignmentModule } from './service-option-assignment/service-option-assignment.module';
+import { UploadsModule } from './uploads/uploads.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UsersModule, GymsModule, ConfigModule.forRoot(), ServicesModule, ServiceOptionModule, GymClassesModule, ServiceOptionAssignmentModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    GymsModule,
+    ConfigModule.forRoot(),
+    UploadsModule,
+    MulterModule.register({ dest: './uploads' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
