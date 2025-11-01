614a7bc0663ecae2f0f99bb489189fd889144b90
berki6 <bereketfikadu71@gmail.com>
2025-11-01 12:45:27 +0300

    feat(upload): add DatabaseModule import to UploadsModule

--- fitness-platform/src/uploads/uploads.module.ts
+++ fitness-platform/src/uploads/uploads.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadsService } from './uploads.service';
 import { UploadsController } from './uploads.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [UploadsService],
   controllers: [UploadsController],
 })
