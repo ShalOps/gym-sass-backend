ef488b767ff8e4529401695769c61f635166e412
ruth610 <ruthambaw610@gmail.com>
2025-11-06 20:57:17 +0300

    gym review update functionality is added

--- fitness-platform/src/reviews/reviews.controller.ts
+++ fitness-platform/src/reviews/reviews.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { ReviewsService } from "./reviews.service";
 import { CreateReviewDto } from "./dto/create-review.dto";
 import { GetUser } from "src/common/decorators/get-user.decorator";
+import { RolesGuard } from "src/auth/guards/roles.guard";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { Role } from "generated/prisma";
+import { UpdateReviewDto } from "./dto/update-review.dto";
 
 @Controller('reviews')
 export class ReviewsController {
@@ -15,6 +19,17 @@ export class ReviewsController {
     @GetUser() user: any,  
     ) {
     return this.reviewsService.createReview(user.userId, dto);
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.CUSTOMER)
+    @Patch(':id')
+    update(
+        @GetUser('userId') userId: number,
+        @Param('id') id: string,
+        @Body() dto: UpdateReviewDto,
+    ) {
+        return this.reviewsService.updateReview(userId, +id, dto);
     }
 
 }
--- fitness-platform/src/reviews/reviews.service.ts
+++ fitness-platform/src/reviews/reviews.service.ts
@@ -1,6 +1,7 @@
 import { ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
 import { DatabaseService } from "src/database/database.service";
 import { CreateReviewDto } from "./dto/create-review.dto";
+import { UpdateReviewDto } from "./dto/update-review.dto";
 
 @Injectable()
 export class ReviewsService {
@@ -18,6 +19,18 @@ export class ReviewsService {
 
     return this.databaseservice.gymReview.create({
       data: { ...dto, userId },
+    });
+  }
+
+  
+  async updateReview(userId: number, reviewId: number, dto: UpdateReviewDto) {
+    const review = await this.databaseservice.gymReview.findUnique({ where: { id: reviewId } });
+    if (!review) throw new NotFoundException('Review not found');
+    if (review.userId !== userId) throw new ForbiddenException('You can edit only your own review');
+
+    return this.databaseservice.gymReview.update({
+      where: { id: reviewId },
+      data: dto,
     });
   }
 }
