b96b7ff9cce675a8214f1af9c6577110fe28aa80
berki6 <bereketfikadu71@gmail.com>
2025-12-04 14:36:54 +0300

    refactor: add NotificationsModule to BookingsModule imports

--- fitness-platform/src/bookings/bookings.module.ts
+++ fitness-platform/src/bookings/bookings.module.ts
@@ -2,13 +2,19 @@ import { Module, forwardRef } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { AuthModule } from '../auth/auth.module';
 import { PaymentsModule } from '../payments/payments.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { ClassBookingsService } from './class-bookings.service';
 import { ServiceBookingsService } from './service-bookings.service';
 import { ClassBookingsController } from './class-bookings.controller';
 import { ServiceBookingsController } from './service-bookings.controller';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, forwardRef(() => PaymentsModule)],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    NotificationsModule,
+    forwardRef(() => PaymentsModule),
+  ],
   controllers: [ClassBookingsController, ServiceBookingsController],
   providers: [ClassBookingsService, ServiceBookingsService],
   exports: [ClassBookingsService, ServiceBookingsService],
