43a98d76173cb6ecc494164046bb1cd4604630de
Ruth Ambaw <ruthambaw610@gmail.com>
2025-12-06 09:38:46 +0300

    email service and module is setted up

--- fitness-platform/src/email/email.service.ts
+++ fitness-platform/src/email/email.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+    constructor(private readonly configService:ConfigService){}
+    emailTransport(){
+        const transporter = nodemailer.createTransport({
+            host: this.configService.get<string>('EMAIL_HOST'),
+            port: this.configService.get<number>('EMAIL_PORT'),
+            secure: false,
+            auth:{
+                user: this.configService.get<string>('EMAIL_USER'),
+                pass: this.configService.get<string>('EMAIL_PASSWORD'),
+            }
+        })
+        return transporter;
+    }
+}
