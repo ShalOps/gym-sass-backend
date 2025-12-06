3743924cbb87893fbd4faf06acf41607ed5ff2c0
Ruth Ambaw <ruthambaw610@gmail.com>
2025-12-06 09:02:00 +0300

    email module structure setted up

--- fitness-platform/src/app.module.ts
+++ fitness-platform/src/app.module.ts
@@ -24,6 +24,7 @@ import { NotificationsModule } from './notifications/notifications.module';
 import { NotificationModule } from './notification/notification.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
@@ -53,6 +54,7 @@ import { APP_GUARD } from '@nestjs/core';
     ScheduleModule.forRoot(),
     NotificationsModule,
     NotificationModule,
+    EmailModule,
   ],
   controllers: [AppController],
   providers: [
