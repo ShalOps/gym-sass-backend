410238998ac08a46a44f9c244d8dc918d52b53e4
Ruth Ambaw <ruthambaw610@gmail.com>
2025-12-08 00:44:44 +0300

    wiring the email to gym booking confirmation

--- fitness-platform/src/notifications/notifications.service.ts
+++ fitness-platform/src/notifications/notifications.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { DateUtil } from '../common/utils/date.util';
+import { EmailService } from 'src/email/email.service';
+import { emoji } from 'zod/mini';
 
 @Injectable()
 export class NotificationsService {
   private readonly logger = new Logger(NotificationsService.name);
+  constructor(private readonly emailService:EmailService){}
 
   async sendEmailReceipt(email: string, amount: number, txRef: string) {
     // TODO: Integrate Nodemailer/SMS
@@ -18,18 +21,40 @@ export class NotificationsService {
       BookingName: string;
       startTime: Date;
       gymName: string;
+      userId: number;
       timezone: string;
     },
   ) {
-    // TODO: Integrate in Both Booking Services
+
     const formattedTime = DateUtil.formatInTimezone(
       bookingDetails.startTime,
       bookingDetails.timezone,
     );
 
-    this.logger.log(
-      `[MOCK EMAIL] Sending booking confirmation to ${email} for "${bookingDetails.BookingName}" at ${formattedTime} (${bookingDetails.gymName})`,
-    );
+        // wanted to mention user name
+    const html = `
+      <h1>🎉 Congratulations, ${bookingDetails.userId}!</h1>
+      <p>
+        You have successfully booked <b>${bookingDetails.BookingName}</b> at
+        <b>${formattedTime}</b> in <b>${bookingDetails.gymName} Gym</b>.
+      </p>
+      <p>We are excited to see you there! 💪</p>
+    `;
+
+    const subject = `Successfully Booked to ${bookingDetails.gymName} Gym, congratulations! 🎉`;
+
+    try {
+
+      await this.emailService.sendEmail({
+        recipients: [email],
+        subject: subject,
+        html: html,
+      });
+
+    } catch (error) {
+      this.logger.error(`Failed to send booking confirmation email to ${email}: ${error.message}`);
+      throw error;
+    }
   }
 
   async notifyUser(userId: number, message: string) {
